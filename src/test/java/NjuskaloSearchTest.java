b2658c6dddc8ecc9e9c03cb09d6087f2ced7bd5a
Mihael Lendvaj <lendvaj.miha@gmail.com>
2025-02-07 17:31:18 +0100

    Resolve NjuskaloSearchTest Typo

--- src/test/java/NjuskaloSearchTest.java
+++ src/test/java/NjuskaloSearchTest.java
@@ -5,7 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
-public class NjustaloSearchTest extends NjuskaloTest{
+public class NjuskaloSearchTest extends NjuskaloTest {
     @Test
     public void njuskaloSearchTest() {
         String searchText = "iPhone 16";
