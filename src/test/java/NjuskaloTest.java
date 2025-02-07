61f67484fbf3fe924cc8ee34c5e411e3a692104b
Mihael Lendvaj <lendvaj.miha@gmail.com>
2025-02-07 15:28:53 +0100

    Add base test class

---
+++ src/test/java/NjuskaloTest.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class NjuskaloTest {
+    public WebDriver driver;
+    public WebDriverWait wait;
+    public String url = "https://www.njuskalo.hr/";
+
+    @BeforeMethod
+    public void setupTest() throws InterruptedException {
+        System.setProperty("webdriver.edge.driver", "drivers/msedgedriver.exe");
+        driver = new EdgeDriver();
+        driver.navigate().to(url);
+        WebElement acceptButton = driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div/div/div[3]/button[2]"));
+        acceptButton.click();
+        wait = new WebDriverWait(driver, 10);
+        WebElement privacyButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[3]/div/div[2]/div[1]/div[2]/button")));
+        privacyButton.click();
+    }
+
+    @AfterMethod
+    public void teardownTest() {
+        driver.quit();
+    }
+}
